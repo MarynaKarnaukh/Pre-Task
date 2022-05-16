301df9edc69386e89896e3ec22658e706d94edeb
MarynaKarnaukh <105630886+MarynaKarnaukh@users.noreply.github.com>
2022-05-16 10:52:57 +0300

    Added Enum

---
+++ EmployeesName.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pre_Task.Enum
+{
+    enum EmployeesName
+    {
+        Anna = 0,
+        Euvgeniia = 1,
+        Mykola =2,
+        Oleksandr = 3,
+        Olena = 4,
+        Sergiy = 5,
+        Viktoriya = 6
+        
+    }
+}
---
+++ ProgrammingLanguages.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pre_Task.Enum
+{
+    enum ProgrammingLanguages
+    {
+        C = 0,
+        Java = 1,
+        JavaScript = 2,
+        Python = 3
+
+    }
+}
