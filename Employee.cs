43dc986a66c02acc3538022ef684dc458d71d0ea
MarynaKarnaukh <105630886+MarynaKarnaukh@users.noreply.github.com>
2022-05-16 10:50:47 +0300

    Added classes Program & Employee

---
+++ Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pre_Task.Enum;
+
+namespace Pre_Task
+{
+    internal class Employee
+    {
+        public EmployeesName Name { get; }
+        public int Age { get; }
+        public ProgrammingLanguages ProgrammingLanguage { get; }
+
+        public Employee()
+        {
+            Random random = new Random();
+            Name = (EmployeesName)random.Next(0, 7);
+            Age = random.Next(18, 40);
+            ProgrammingLanguage = (ProgrammingLanguages)random.Next(0, 4);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Employye's name is {0}, age is {1}, programming language is {2}", Name, Age, ProgrammingLanguage);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Pre_Task;
+using System.Linq;
+
+Employee first_employee = new();
+Employee second_employee = new();
+Employee third_employee = new();
+
+List<Employee> employees = new();
+employees.Add(first_employee);
+employees.Add(second_employee);
+employees.Add(third_employee);
+
+Action < List<Employee> > Print = employees => { foreach (Employee employee in employees) Console.WriteLine(employee); };
+
+Print(employees);
+
+Func<List<Employee>, List<Employee>> ToSortByAge = eployees => employees.OrderByDescending(employee => employee.Age).ToList();
+
+Console.WriteLine("Sorting by age : ");
+Print(ToSortByAge(employees));
+
+Func < List<Employee>, List<Employee> > ToSortByName = eployees => employees.OrderBy(employee => employee.Name).ToList();
+
+Console.WriteLine("Sorting by name : ");
+Print(ToSortByName(employees));
+
